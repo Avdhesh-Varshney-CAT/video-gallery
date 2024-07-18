4814306fb3c597137e1eefa7cd1ec1ce90c92163
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-07-18 18:58:12 +0530

    data-struct-update

--- data/file.py
+++ data/file.py
@@ -16,24 +16,25 @@ def jsonUpdate():
     json_data = []
     for i in range(len(data)):
         dic = {
-            "ID": data[str(i)]["ID"],
-            "name": data[str(i)]["name"],
-            "imageURL": data[str(i)]["imageURL"],
-            "timeStamp": data[str(i)]["timeStamp"],
-            "exactTimeLength": data[str(i)]["exactTimeLength"],
-            "tags": data[str(i)]['tags'],
-            "iFrameURL": data[str(i)]["iFrameURL"],
-            "videoURL": data[str(i)]["videoURL"],
-            "downloadURL": data[str(i)]['downloadURL'],
-            "websiteName": data[str(i)]["websiteName"],
-            "websiteURL": data[str(i)]["websiteURL"],
+            "ID": data[i]["ID"],
+            "name": data[i]["name"],
+            "boyName": data[i]['boyName'],
+            "girlName": data[i]['girlName'],
+            "imageURL": data[i]["imageURL"],
+            "timeStamp": data[i]["timeStamp"],
+            "tags": data[i]['tags'],
+            "iFrameURL": data[i]["iFrameURL"],
+            "videoURL": data[i]["videoURL"],
+            "downloadURL": data[i]['downloadURL'],
+            "websiteName": data[i]["websiteName"],
+            "websiteURL": data[i]["websiteURL"],
         }
         json_data.append(dic)
 
-    # print(json_data[0])
+    print(json_data[0])
 
-    with open(FILE_PATH, 'w') as f:
-        json.dump(json_data, f, indent=2)
+    # with open(FILE_PATH, 'w') as f:
+    #     json.dump(json_data, f, indent=2)
 
 if __name__ == '__main__':
     jsonUpdate()
